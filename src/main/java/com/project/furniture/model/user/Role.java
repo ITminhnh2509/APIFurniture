c2c14a684220ea1f07ee6220b20065dabeb47100
ITminhnh2509 <minhnh2509@gmail.com>
2024-09-23 14:47:25 +0700

    change String role to Role with ID

--- src/main/java/com/project/furniture/model/user/Role.java
+++ src/main/java/com/project/furniture/model/user/Role.java
@@ -1,25 +1,29 @@
-//package com.project.furniture.model.user;
-//
-//import com.project.furniture.model.BaseEntity;
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Setter
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Entity
-//@Data
-//@Table(name="roles")
-//@Builder
-//public class Role {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(nullable = false)
-//    private String name;
-//
-//    public static String ADMIN = "ADMIN";
-//    public static String USER = "USER";
-//}
+package com.project.furniture.model.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name="roles")
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private Set<User> users;
+    public static String ADMIN = "ROLE_ADMIN";
+    public static String USER = "ROLE_USER";
+}
