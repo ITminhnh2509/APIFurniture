8a1851819931d91c17c84deabdf935f0452bcc29
ITminhnh2509 <minhnh2509@gmail.com>
2024-09-24 16:19:26 +0700

    finish cors

--- src/main/java/com/project/furniture/config/WebConfig.java
+++ src/main/java/com/project/furniture/config/WebConfig.java
@@ -1,4 +1,21 @@
-//package com.project.furniture.config;
+package com.project.furniture.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow React app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow necessary methods
+                .allowCredentials(true);
+    }
+}
+
 //
 //
 //import org.springframework.boot.web.servlet.FilterRegistrationBean;
