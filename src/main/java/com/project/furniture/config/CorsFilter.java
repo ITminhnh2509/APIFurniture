a2da510fc2f443a9d5d51b8c76773c98985362e2
ITminhnh2509 <minhnh2509@gmail.com>
2024-09-26 23:02:13 +0700

    add role into token

--- src/main/java/com/project/furniture/config/CorsFilter.java
+++ src/main/java/com/project/furniture/config/CorsFilter.java
@@ -13,9 +13,19 @@ import java.io.IOException;
 @Component("customCorsFilter")
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter extends OncePerRequestFilter {
+
+    private static final String ALLOWED_ORIGIN_1 = "http://localhost:3000";
+    private static final String ALLOWED_ORIGIN_2 = "http://localhost:3001";
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        String origin = request.getHeader("Origin");
+
+        // Check if the origin is one of the allowed origins
+        if (ALLOWED_ORIGIN_1.equals(origin) || ALLOWED_ORIGIN_2.equals(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
--- src/main/java/com/project/furniture/config/JwtToken.java
+++ src/main/java/com/project/furniture/config/JwtToken.java
@@ -1,6 +1,5 @@
 package com.project.furniture.config;
 
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -23,41 +22,32 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class JwtToken {
     @Value("${jwt.expiration}")
-    private int expiration; //save to an environment variable
+    private int expiration; // save to an environment variable
     @Value("${jwt.secretKey}")
     private String secretKey;
-    public String generateToken(com.project.furniture.model.user.User user) throws Exception{
-        //properties => claims
+
+    public String generateToken(com.project.furniture.model.user.User user) throws Exception {
         Map<String, Object> claims = new HashMap<>();
-        //this.generateSecretKey();
         claims.put("username", user.getUsername());
+        claims.put("role", user.getRole().getName()); // Add role name here
+
         try {
-            String token = Jwts.builder()
-                    .setClaims(claims) //how to extract claims from this ?
+            return Jwts.builder()
+                    .setClaims(claims)
                     .setSubject(user.getUsername())
                     .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
                     .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                     .compact();
-            return token;
-        }catch (Exception e) {
-            //you can "inject" Logger, instead System.out.println
-            throw new Exception("Cannot create jwt token, error: "+e.getMessage());
-
-            //return null;
+        } catch (Exception e) {
+            throw new Exception("Cannot create JWT token, error: " + e.getMessage());
         }
     }
+
     private Key getSignInKey() {
         byte[] bytes = Decoders.BASE64.decode(secretKey);
-        //Keys.hmacShaKeyFor(Decoders.BASE64.decode("TaqlmGv1iEDMRiFp/pHuID1+T84IABfuA0xXh4GhiUI="));
         return Keys.hmacShaKeyFor(bytes);
     }
-    private String generateSecretKey() {
-        SecureRandom random = new SecureRandom();
-        byte[] keyBytes = new byte[32]; // 256-bit key
-        random.nextBytes(keyBytes);
-        String secretKey = Encoders.BASE64.encode(keyBytes);
-        return secretKey;
-    }
+
     private Claims extractAllClaims(String token) {
         try {
             return Jwts.parser()
@@ -66,26 +56,34 @@ public class JwtToken {
                     .parseClaimsJws(token)
                     .getBody();
         } catch (Exception e) {
-            // Xử lý lỗi nếu token không hợp lệ hoặc bị lỗi
             throw new RuntimeException("Invalid token");
         }
     }
-    public  <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = this.extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
-    //check expiration
+
+    public Claims extractClaims(String token) {
+        return extractAllClaims(token);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
+    }
+
     public boolean isTokenExpired(String token) {
         Date expirationDate = this.extractClaim(token, Claims::getExpiration);
         return expirationDate.before(new Date());
     }
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-    public boolean validateToken(String token, UserDetails userDetails) {
-        String phoneNumber = extractUsername(token);
-        return (phoneNumber.equals(userDetails.getUsername()))
-                && !isTokenExpired(token);
-    }
 
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
 }
